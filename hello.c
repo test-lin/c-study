b59b894014d2972f68a9777547f3a707fb7228b3
jani <505932384@qq.com>
2019-06-29 23:09:12 +0800

    注释

--- hello.c
+++ hello.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
+/**
+ * 第一个实例
+*/
 int main(int argc, char const *argv[])
 {
     printf("hello %s!!!\n", "jani");
 
     return 0;
 }
+
+// 单选注释
+
+/*
+多行注释
+多行注释
+多行注释
+多行注释
+*/
